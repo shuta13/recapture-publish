9a7d0e3c7f4278d60a0145627f4ba5cdd7c6439a
shuta13 <hollystarsun@gmail.com>
2019-11-05 14:04:08 +0900

    :sparkles: aboutのページを実装

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,16 +3,40 @@ import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { useUpdateComponentAnimate } from '../store/Actions';
 
+import icon from '../assets/images/icon.svg';
+
 const durationAnimate = 200;
 
-const AppAbout = styled.div`
+const WrapAbout = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-  color: #ffffff;
 `;
+
+const AppAbout = styled.div`
+  width: 360px;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+`
+const AboutImage = styled.img`
+  width: 160px;
+  height: 160px;
+`
+
+const AboutDescription = styled.div`
+  width: 320px;
+  height: 360px;
+  color: #fff;
+  font-family: Noto Serif JP;
+  font-size: 12px;
+
+  margin-top: 20px;
+`
 
 const About: React.FC = () => {
   const animate = useSelector(
@@ -24,7 +48,23 @@ const About: React.FC = () => {
       if (!animate) updateComponentAnimate();
     }, durationAnimate)
   }, [])
-  return <AppAbout>うんちぷり</AppAbout>;
+  return (
+    <WrapAbout>
+      <AppAbout>
+        <AboutImage src={icon}></AboutImage>
+        <AboutDescription>
+          <p>1999年京都生まれ，京都在住．</p>
+          <p>主にWebフロントエンドにおけるSPA開発やUI/UXデザインを行っています．</p>
+          <p>音楽・お酒を特に好みます．</p>
+          <br></br>
+          <p>立命館大学情報理工学部情報理工学科画像・音メディアコース 所属</p>
+          <p>ElevenBack LLC フロントエンドエンジニア・デザイナー</p>
+          <p>株式会社タンバリン フロントエンドエンジニア</p>
+          <p>株式会社計数技研 エンジニアアルバイト</p>
+        </AboutDescription>
+      </AppAbout>
+    </WrapAbout>
+  );
 };
 
 export default About;
