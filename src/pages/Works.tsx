68f6ae6f0688c5593e9f9b0e42934769ba19e277
shuta13 <hollystarsun@gmail.com>
2019-11-06 12:26:25 +0900

    :label: react-scroll-horizontalのなんちゃって型定義を追加

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import HorizontalScroll from 'react-scroll-horizontal';
 
 const Works: React.FC = () => {
-  return <div></div>;
+  const child   = { width: `30em`, height: `100%`}
+  const parent  = { width: `60em`, height: `100%`}
+  return (
+    <div style={parent}>
+      <HorizontalScroll>
+          <div style={child} />
+          <div style={child} />
+          <div style={child} />
+      </HorizontalScroll>
+    </div>
+  );
 };
 
 export default Works;
