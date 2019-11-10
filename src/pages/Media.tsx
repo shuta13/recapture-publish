944827f8e94c2f415d4a4077b5c5d3c77e91b5c8
shuta13 <hollystarsun@gmail.com>
2019-11-10 20:33:52 +0900

    mediaはまだwip

--- src/pages/Media.tsx
+++ src/pages/Media.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const AppMedia = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 const Media: React.FC = () => {
-  return <div></div>;
+  return <AppMedia>coming soon</AppMedia>;
 };
 
 export default Media;
