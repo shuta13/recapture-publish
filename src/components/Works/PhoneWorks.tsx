d9a0e1967ce5f3bb8e8be663e1e10da1c4ab568d
shuta13 <hollystarsun@gmail.com>
2019-12-03 12:44:36 +0900

    wip: worksのwip表示

--- src/components/Works/PhoneWorks.tsx
+++ src/components/Works/PhoneWorks.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const AppWorks = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: #1d1d1d;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #ffffff;
+`;
 
 const PhoneWorks: React.FC = () => {
-  return <div>スマホ画面</div>;
+  return (
+    <AppWorks>
+      works for phone is coming soon
+    </AppWorks>
+  );
 };
 
 export default PhoneWorks;
